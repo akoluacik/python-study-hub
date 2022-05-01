8244045f2fc27ad2dfbdbdc2fd93f29de763df34
akoluacik <anilkoluacik1999@gmail.com>
2022-05-02 00:33:03 +0300

    Decision Tree Classifier added

---
+++ Machine Learning with Python/3.Classification Algorithms/5.DecisionTree.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 23:46:21 2022
+
+@author: anilk
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+
+# Read data
+data = pd.read_csv("dataset.csv")
+
+x=data.iloc[:,1:4].values
+y=data.iloc[:,4:].values
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=0.67, random_state=0)
+
+#Decision Tree Classifier Model
+dtc = DecisionTreeClassifier(criterion='entropy')
+dtc.fit(X_train, y_train)
+
+#Predict
+y_pred=dtc.predict(X_test)
+
+#Confusion Matrix
+cm=confusion_matrix(y_test, y_pred)
+print(cm)
